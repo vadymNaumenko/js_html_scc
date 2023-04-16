476a9ddb40623779225a10e3edfdb76c4186a1d3
Vdym <vadimnaumnaum@gmail.com>
2023-04-16 02:05:56 +0200

    form with JS

---
+++ formWithJavaScript/index.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('id-name');
+const form2 = document.getElementById('jj');
+console.log(form);
+console.log(form2);
+
+const butn = document.getElementById('button'); 
+console.log(butn);
+
+butn.addEventListener('click' , ()=>{
+console.log(form);
+});
